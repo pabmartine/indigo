9464a0f933581e4f38c466907766c3de9f09e556
Pablo Martín Escribano <pablo.martin-escribano@capgemini.es>
2021-10-27 09:33:58 +0200

    no message

--- indigo-frontend/src/environments/environment.prod.ts
+++ indigo-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  endpoint: 'http://indigo-backend/rest/',
+  endpoint: 'http://192.168.1.40/rest/',
 };
--- indigo-frontend/src/environments/environment.ts
+++ indigo-frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  endpoint: 'http://indigo-backend/rest/',
+  endpoint: 'http://192.168.1.40/rest/',
 };
