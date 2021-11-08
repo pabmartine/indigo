46421139ae7662ea06497bed71133a23e0c6e58b
Pablo Martín Escribano <pablo.martin-escribano@capgemini.es>
2021-11-08 07:19:11 +0100

    no message

--- indigo-frontend/src/environments/environment.ts
+++ indigo-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  endpoint: 'http://localhost:8080/rest/',
+  endpoint: '/rest/',
   whiteList: ["localhost:8080", "127.0.0.1:8080", "localhost:8081", "127.0.0.1:8081", "192.168.1.40:8081", "krahen.synology.me:8081"],
   blackList: ["localhost:8080/rest/login", "127.0.0.1:8080/rest/login","localhost:8080/rest/login", "127.0.0.1:8081/rest/login", "192.168.1.40:8081/rest/login", "krahen.synology.me:8080/rest/login"]
 };
