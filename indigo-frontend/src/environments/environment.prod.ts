e1a293ce190778d7318d58e5c75da5b45c9e527d
pmescribano <pabmartine@gmail.com>
2023-01-08 18:37:53 +0100

    test url conection

--- indigo-frontend/src/environments/environment.prod.ts
+++ indigo-frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  endpoint: 'http://192.168.1.40:8081/rest/',
-  whiteList: ["localhost:8080", "127.0.0.1:8080", "localhost:8081", "127.0.0.1:8081", "192.168.1.40:8081", "krahen.synology.me:8081"],
-  blackList: ["localhost:8080/rest/login", "127.0.0.1:8080/rest/login","localhost:8080/rest/login", "127.0.0.1:8081/rest/login", "192.168.1.40:8080/rest/login", "krahen.synology.me:8080/rest/login"]
+  endpoint: 'http://krahen.synology.me:8081/rest/',
+  whiteList: ["localhost", "127.0.0.1", "192.168.1.40", "krahen.synology.me"],
+  blackList: []
 };
