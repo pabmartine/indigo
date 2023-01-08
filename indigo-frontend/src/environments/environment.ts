76d80a82324fc6cb81267a0c008cdaccc5d8008d
pmescribano <pabmartine@gmail.com>
2023-01-08 18:50:44 +0100

    test url conection

--- indigo-frontend/src/environments/environment.ts
+++ indigo-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   endpoint: 'http://krahen.synology.me:8081/rest/',
-  whiteList: ["localhost", "127.0.0.1", "192.168.1.40", "krahen.synology.me"],
+  whiteList: ["localhost:8081", "127.0.0.1:8081", "192.168.1.40:8081", "krahen.synology.me:8081"],
   blackList: []
 };
