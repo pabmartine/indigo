c909892af03eae6538af2c14a24f20d2df89aebe
Pablo Martín Escribano <pablo.martin@io.builders>
2023-05-18 17:22:16 +0200

    new port-adapter-usecase structure for configuration

--- indigo-backend/src/test/java/com/martinia/indigo/common/mail/infrastructure/api/SendMailControllerTest.java
+++ indigo-backend/src/test/java/com/martinia/indigo/common/mail/infrastructure/api/SendMailControllerTest.java
@@ -5,36 +5,63 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import javax.annotation.Resource;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-class SendMailControllerTest {
+public class SendMailControllerTest {
+
+	@MockBean
+	private SendMailUseCase useCase;
 
 	@Resource
 	private MockMvc mockMvc;
 
-	@MockBean
-	private SendMailUseCase sendMailUseCase;
-
 	@Test
-	void testSendMail_Success() throws Exception {
-		String path = "testPath";
-		String address = "test@example.com";
+	public void testSend_When_MailSentSuccessfully_Then_ReturnOkStatus() throws Exception {
+		// Given
+		String path = "samplePath";
+		String address = "sampleAddress";
 
-		mockMvc.perform(get("/rest/mail/send").param("path", path).param("address", address)).andExpect(status().isOk());
+		when(useCase.mail(eq(path), eq(address))).thenReturn(null);
 
-		verify(sendMailUseCase).mail(path, address);
+		// When-Then
+		mockMvc.perform(get("/rest/mail/send").param("path", path).param("address", address).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+
+		verify(useCase, times(1)).mail(eq(path), eq(address));
 	}
 
-}
+	@Test
+	public void testSend_When_MailSendingFailed_Then_ThrowException() throws Exception {
+		// Given
+		String path = "samplePath";
+		String address = "sampleAddress";
+		String error = "Mail sending failed";
 
+		when(useCase.mail(eq(path), eq(address))).thenReturn(error);
+
+		// When-Then
+		mockMvc.perform(get("/rest/mail/send").param("path", path).param("address", address).contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isInternalServerError())
+				.andExpect(result -> assertTrue(result.getResolvedException() instanceof Exception))
+				.andExpect(result -> assertEquals(error, result.getResolvedException().getMessage()));
+
+		verify(useCase, times(1)).mail(eq(path), eq(address));
+	}
+}
 
 
 
