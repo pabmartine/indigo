6a7ef0b1060f5d2f7c391c02b74fe677e45fd7e8
Pablo Martín Escribano <pablo.martin@io.builders>
2023-06-11 17:28:37 +0200

    new port-adapter-usecase structure for notification

--- indigo-frontend/src/environments/environment.ts
+++ indigo-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  endpoint: 'http://localhost:8080/rest/',
+  endpoint: 'http://krahen.synology.me:8081/rest/',
   whiteList: ["localhost:8080", "localhost:8081", "127.0.0.1:8081", "192.168.1.40:8081", "krahen.synology.me:8081"],
   blackList: []
 };
