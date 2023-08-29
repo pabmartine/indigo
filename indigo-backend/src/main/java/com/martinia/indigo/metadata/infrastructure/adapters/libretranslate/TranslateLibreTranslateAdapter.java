1693a7eccd50088375163125add30b0baf8f905f
Pablo Martín Escribano <pablo.martin@io.builders>
2023-08-29 08:09:48 +0200

    enable libretranslate

--- indigo-backend/src/main/java/com/martinia/indigo/metadata/infrastructure/adapters/libretranslate/TranslateLibreTranslateAdapter.java
+++ indigo-backend/src/main/java/com/martinia/indigo/metadata/infrastructure/adapters/libretranslate/TranslateLibreTranslateAdapter.java
@@ -2,6 +2,7 @@ package com.martinia.indigo.metadata.infrastructure.adapters.libretranslate;
 
 import com.martinia.indigo.metadata.domain.ports.adapters.libretranslate.TranslateLibreTranslatePort;
 import com.martinia.indigo.metadata.domain.ports.usecases.libretranslate.TranslateLibreTranslateUseCase;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,7 @@ import javax.annotation.Resource;
 
 @Component
 @ConditionalOnProperty(name = "flags.libretranslate", havingValue = "true")
+@Slf4j
 public class TranslateLibreTranslateAdapter implements TranslateLibreTranslatePort {
 
 	@Resource
@@ -16,6 +18,8 @@ public class TranslateLibreTranslateAdapter implements TranslateLibreTranslatePo
 
 	@Override
 	public String translate(final String text, final String target) {
-		return useCase.translate(text, target);
+		final String comment = useCase.translate(text, target);
+		log.debug("Comment {} translated to {} as {}", text, target, comment);
+		return comment;
 	}
 }
