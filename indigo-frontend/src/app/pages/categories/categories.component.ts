6604aa49c65c86a899eac5001bd21e357b9f26f2
Pablo Martín Escribano <pablo.martin@io.builders>
2025-11-02 12:40:19 +0100

    feat: refactor authentication state management, remove ngx-infinite-scroll, and update UI components

--- indigo-frontend/src/app/pages/categories/categories.component.ts
+++ indigo-frontend/src/app/pages/categories/categories.component.ts
@@ -20,6 +20,7 @@ interface TagWithTempImage extends Tag {
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
+  styleUrls: ['./categories.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   providers: [MessageService]
 })
