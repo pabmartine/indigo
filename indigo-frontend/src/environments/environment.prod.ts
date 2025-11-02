d716b676c34052f6f9ce2799f03e6854f3da096b
Pablo Martín Escribano <pablo.martin@io.builders>
2025-11-02 19:48:34 +0100

    feat: update build configuration and add debug output for environment variables

--- indigo-frontend/src/environments/environment.prod.ts
+++ indigo-frontend/src/environments/environment.prod.ts
@@ -1,14 +1,8 @@
-// environment.ts
-declare const process: any;
-
+// environment.prod.ts - Para producción
 export const environment = {
-  production: false,
-  endpoint: process.env['NG_APP_API_ENDPOINT'],
-  internalEndpoint: process.env['NG_APP_INTERNAL_ENDPOINT'],
-  whiteList: process.env['NG_APP_WHITELIST']
-    ? process.env['NG_APP_WHITELIST'].split(',').map(item => item.trim())
-    : [],
-  blackList: process.env['NG_APP_BLACKLIST']
-    ? process.env['NG_APP_BLACKLIST'].split(',').map(item => item.trim())
-    : []
+  production: true,
+  endpoint: '__NG_APP_API_ENDPOINT__',
+  internalEndpoint: '__NG_APP_INTERNAL_ENDPOINT__',
+  whiteList: '__NG_APP_WHITELIST__'.split(',').map(item => item.trim()).filter(item => item.length > 0),
+  blackList: '__NG_APP_BLACKLIST__'.split(',').map(item => item.trim()).filter(item => item.length > 0)
 };
